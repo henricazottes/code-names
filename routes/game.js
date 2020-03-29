92dd301183afdde612bb0a1a1a47e755ffd42de1
Henri Cazottes <henri.cazottes@protonmail.com>
2020-03-29 19:49:21 +0200

    [fix] remove fb tracking url parameter

--- routes/game.js
+++ routes/game.js
@@ -2,10 +2,17 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/:id', function(req, res) {
-  res.render('game', {
-    gameId: req.params.gameId,
-    status: 'public'
-  })
+  if(req.originalUrl.includes('?fbclid=')) {
+    const cleanUrl = decodeURI(req.originalUrl)
+      .replace(/\?fbclid=.+/g, '')
+      .replace(/ /g, '_')
+    return res.redirect(cleanUrl)
+  } else {
+    res.render('game', {
+      gameId: req.params.gameId,
+      status: 'public'
+    })
+  }
 })
 
 router.post('/:id', function(req, res) {
